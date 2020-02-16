08350263dc2b36c6fedb9b7122f5c0e6f8c93008
joanshen0508 <joanshen0508@gmail.com>
2020-02-16 17:42:13 +0400

    123123123

---
+++ KTS-master/setup.py
@@ -0,0 +1,7 @@
+# cd ../from distutils.core import setup, Extension
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+ext = Extension('cpd_nonlin', sources=['cpd_nonlin.pyx'], include_dirs=['.', np.get_include()])
+setup(name='cpd_nonlin', ext_modules=cythonize([ext]))
